db11a91ee7800067808bd82f1cfd8d3ea066526d
David Martin <->
2012-07-16 21:09:24 +0100

    Add cross device event handling. Added colour/shape delegation per user

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -8,39 +8,64 @@ socket.on('registered', function (data) {
 
 // broadcast reception
 var counter = 0;
-var shapes = ['circle', 'triangle', 'square'];
 socket.on('message', function (data) {
   var id = (counter += 1);
-  console.log('id=' + id);
-  x$('body').top('<div id="' + id + '" class="' + shapes[(id%shapes.length)] + '"></div>');
+  //console.log('id=' + id);
+  x$('body').top('<div id="' + id + '" class="' + data.shape + '"></div>');
   var idDiv = x$('#' + id);
-  console.log('added div');
-  idDiv.css({
+  //console.log('added div');
+  var css = {
     "left":  data.x + 'px',
     "top": data.y + 'px'
-  });
-  console.log('updated css');
+  };
+  if ('triangle' === data.shape) {
+    css['border-bottom-color'] = data.colour;
+  } else {
+    css['background-color'] = data.colour;
+  }
+  idDiv.css(css);
+  //console.log('updated css');
   //idDiv.addClass('animate');
   setTimeout(function () {
-    console.log('adding animate class');
+    //console.log('adding animate class');
     idDiv.addClass('animate');
     setTimeout(function () {
-      console.log('removing');
+      //console.log('removing');
       idDiv.remove();
     }, 500);
   }, 10);
 });
 
-x$('body').on('touchend', function (e) {
-  console.log('touched');
-  for (var key in e) {
-    console.log('e.' + key + '=' + e[key]);
-  }
-  console.log('e.changedTouches=' + JSON.stringify(e.changedTouches));
-  console.log('e.touches=' + JSON.stringify(e.touches));
-  console.log('e.targetTouches=' + JSON.stringify(e.targetTouches));
+function sendEvent(x, y) {
+  //console.log('touched@' + x + ',' + y);
   socket.emit('message', {
-    "x": (e.clientX - 10),
-    "y": (e.clientY - 10)
+    "x": x - 10,
+    "y": y - 10
   });
+}
+
+function isTouchDevice() {
+   var el = document.createElement('div');
+   el.setAttribute('ongesturestart', 'return;');
+   var gestureType = typeof el.ongesturestart;
+   if('function' === gestureType) {
+      return true;
+   }
+   return false;
+}
+
+var touchEvent = isTouchDevice() ? 'touchstart' : 'click';
+
+x$('body').on(touchEvent, function (e) {
+  var x,y;
+
+  if ('undefined' !== typeof e.touches) {
+    x = e.touches[0].pageX;
+    y = e.touches[0].pageY;
+  } else {
+    x = e.clientX;
+    y = e.clientY;
+  }
+
+  sendEvent(x, y);
 });
--- cloud/application.js
+++ cloud/application.js
@@ -8,39 +8,43 @@ var staticPath = path.normalize(__dirname + "/../client/default");
 console.log('staticPath=' + staticPath);
 app.use(express.static(staticPath));
 
+var shapes = ['circle', 'triangle', 'square'];
+var shapeCounter = 0;
+var colours = ['#000', '#f33', '#3f3', '#33f', '#ff3', '#3ff', '#f3f', '#f99', '#9f9', '#99f', '#ff9', '#9ff', '#f9f', '#993', '#933', '#393', '#339', '#399', '#999', '#666'];
+var colourCounter = 0;
+
+function nextShape() {
+  return shapes[(shapeCounter += 1) % shapes.length];
+}
+
+function nextColour() {
+  return colours[(colourCounter += 1) % colours.length];
+}
+
 io.sockets.on('connection', function(socket) {
-  // var username;
+  var shape;
+  var colour;
+
+  shape = nextShape();
+  colour = nextColour();
+
   socket.join('room');
 
   // Broadcast messages to all clients
   socket.on('message', function(data) {
-    // if (!username) {
-    //   socket.join('room');
-    //   socket.emit('registered', {
-    //     message: 'connected to server'
-    //   });
-
-    //   // User is registering.
-    //   // Add them to room and emit connected message
-    //   // Also send an update to all clients to say this user is connected
-    //   username = data.message;
-    //   // io.sockets.in('room').emit('message', {
-    //   //   message: username + ' connected'
-    //   // });
-    //   console.log(username + ' connected');
-    // } else {
-      // User is sending message. Emit to all clients
-      io.sockets.in('room').emit('message', data);
-      console.log('data=' + JSON.stringify(data));
-    // }
+    var message = {
+      "x": data.x,
+      "y": data.y,
+      "shape": shape,
+      "colour": colour
+    };
+    io.sockets.in('room').emit('message', message);
+    console.log('data=' + JSON.stringify(message));
   });
 
   // If user disconnects
   socket.on('disconnect', function() {
     var message = socket.id + ' disconnected';
-    // io.sockets.in('room').emit('message', {
-    //   message: message
-    // });
     console.log(message);
   });
 });
