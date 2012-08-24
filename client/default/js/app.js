40f2974575aa8aa94ac04fee52625667ad3c19a9
John Frizelle <johnfriz@gmail.com>
2012-08-24 11:34:07 +0200

    Update client/default/js/app.js

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -1,4 +1,4 @@
-var url = ($fh.APP_MODE_RELEASE === $fh.legacy.appMode ? $fh.legacy.releaseCloudUrl : $fh.legacy.debugCloudUrl);
+var url = ($fh.app_props.mode === "dev" ? $fh.cloud_props.hosts.debugCloudUrl : $fh.cloud_props.hosts.releaseCloudUrl);
 document.getElementById('serverurl').innerHTML = 'Server URL: ' + url;
 var socket = io.connect(url);
 
